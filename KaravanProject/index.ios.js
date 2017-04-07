fcab054bffe3a62fe6246e9b30b5b2fb2b2785d1
DereK Han <derekhan206@gmail.com>
2017-04-07 11:12:26 -0700

    adds navigation functinoality

--- KaravanProject/index.ios.js
+++ KaravanProject/index.ios.js
@@ -15,33 +15,50 @@ import {
 
 import Login from './src/screens/Login';
 import Secured from './src/screens/Secured';
+import NextScreen from './src/screens/NextScreen';
 
 var dataset = require("./src/data/mockdata.json");
 
 class KaravanProject extends Component {
   state = {
-    isLoggedIn: false
+    isLoggedIn: false,
+    screenName: "Home"
   }
 
   _attemptLogin = (object) => {
     for(var person of dataset) {
       if(person.username == object.username) {
         if(person.password == object.password) {
-          this.setState({ isLoggedIn: true })
+          this.setState({ screenName: "Home" });
+          this.setState({ isLoggedIn: true });
         } else {
           object.message = "Invalid username and password combination.";
-          this.forceUpdate()
+          this.forceUpdate();
         }
       } else {
         object.message = "Invalid username and password combination.";
-        this.forceUpdate()
+        this.forceUpdate();
       }
     }
   }
 
+  _navigateToNewScreen = (screen) => {
+    this.setState({screenName: screen.toString()});
+  }
+
   render() {
     if (this.state.isLoggedIn) {
-      return <Secured onLogoutPress={() => this.setState({isLoggedIn: false})} />;
+      if (this.state.screenName == "Home") {
+        return <Secured 
+            onLogoutPress={() => this.setState({isLoggedIn: false})}
+            navigateButton={(screenName) => this._navigateToNewScreen(screenName)}
+          />;
+      } else if (this.state.screenName == "Map") {
+        return <NextScreen 
+             onLogoutPress={() => this.setState({isLoggedIn: false})}
+             navigateButton={(screenName) => this._navigateToNewScreen(screenName)}
+          />
+      }
     } else {
       return <Login onLoginPress={(object) => this._attemptLogin(object)} />;
     }
